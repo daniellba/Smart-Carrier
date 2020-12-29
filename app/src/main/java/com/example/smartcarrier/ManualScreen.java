37803735f3ff6332968c94451168ba094956ad92
daniellbn <benami1301@gmail.com>
2020-12-29 14:52:23 +0200

    Added splash to the app

--- app/src/main/java/com/example/smartcarrier/ManualScreen.java
+++ app/src/main/java/com/example/smartcarrier/ManualScreen.java
@@ -1,13 +1,14 @@
 package com.example.smartcarrier;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ManualScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_manual_screen);
     }
 }
