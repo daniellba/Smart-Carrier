a1f97cb9516e5cfc59e56e3514380713f3527c40
daniellbn <benami1301@gmail.com>
2021-05-27 17:47:27 +0300

    Adjustments

--- app/src/main/java/com/example/smartcarrier/ManualScreen.java
+++ app/src/main/java/com/example/smartcarrier/ManualScreen.java
@@ -1,10 +1,9 @@
 package com.example.smartcarrier;
-//package net.bane.bluetoothapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -14,68 +13,86 @@ import java.io.OutputStream;
 import java.util.UUID;
 
 public class ManualScreen extends AppCompatActivity {
-
+    BluetoothAdapter btAdapter= null;
+    BluetoothSocket btSocket = null;
     static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    Button Forward_BTN, Backward_BTN, Left_BTN, Right_BTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_screen);
 
-        Button Forward_BTN, Backward_BTN, Left_BTN, Right_BTN;
+        Forward_BTN = findViewById(R.id.Forward_BTN);
 
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter(); //Bluetooth definition
-        //System.out.println(btAdapter.getBondedDevices());
+        //btAdapter = BluetoothAdapter.getDefaultAdapter(); //Bluetooth definition
+    //    System.out.println(btAdapter.getBondedDevices());
 
-        BluetoothDevice hc05 = btAdapter.getRemoteDevice("98:D3:51:F5:B4:73"); //connect to my hc-05 via mac adress
-        //System.out.println(hc05.getName());
+     //   System.out.println(hc05.getName());
 
-        BluetoothSocket btSocket = null;
-        int counter = 0;
+   /*     int counter = 0;
         do {    //creating the socket, if fail, try twice more
             try {
                 btSocket = hc05.createRfcommSocketToServiceRecord(mUUID);
-                //System.out.println(btSocket);
+                System.out.println(btSocket);
                 btSocket.connect();
-                //System.out.println(btSocket.isConnected());
+                System.out.println(btSocket.isConnected());
             } catch (IOException e) {
                 e.printStackTrace();
             }
             counter++;
-        } while (!btSocket.isConnected() && counter < 3);
-
+        } while (!btSocket.isConnected() && counter < 3);*/
 
         try {
-            OutputStream outputStream = btSocket.getOutputStream(); //sending data to BT
-            outputStream.write(48);/////////////////////////////657777777
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            if (btSocket == null) {
+                btAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        InputStream inputStream = null; //receiving BT data
-        try {
-            inputStream = btSocket.getInputStream();
-            inputStream.skip(inputStream.available());
-
-            for (int i = 0; i < 26; i++) {
-
-                byte b = (byte) inputStream.read(); //reading the data byte by byte
-                System.out.println((char) b);
-
+                BluetoothDevice hc05 = btAdapter.getRemoteDevice("98:D3:51:F5:B4:73"); //connect to my hc-05 via mac adress
+                btSocket = hc05.createInsecureRfcommSocketToServiceRecord(mUUID);
+                BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                btSocket.connect();
             }
-
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
 
+        BluetoothSocket finalBtSocket = btSocket;
+        Forward_BTN.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+            try {
+                OutputStream outputStream = finalBtSocket.getOutputStream();
+                outputStream.write(1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            }
+        });
 
-        try {
+//     InputStream inputStream = null; //receiving BT data
+//        try {
+//            inputStream = btSocket.getInputStream();
+//            inputStream.skip(inputStream.available());
+//
+//            for (int i = 0; i < 26; i++) {
+//
+//                byte b = (byte) inputStream.read();
+//                System.out.println((char) b);
+//
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+     /*   try {
             btSocket.close();
             System.out.println(btSocket.isConnected());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
+*/
     }
 }
