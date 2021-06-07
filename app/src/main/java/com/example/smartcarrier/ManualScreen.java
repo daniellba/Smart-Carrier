af8e7f1361f400dd627b1c24ffe767268b68f7d7
daniellbn <benami1301@gmail.com>
2021-06-07 15:51:37 +0300

    Manual screen working buttons
    done defining and reacting buttons

--- app/src/main/java/com/example/smartcarrier/ManualScreen.java
+++ app/src/main/java/com/example/smartcarrier/ManualScreen.java
@@ -3,6 +3,7 @@ package com.example.smartcarrier;
 import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.bluetooth.BluetoothDevice;
@@ -24,13 +25,16 @@ public class ManualScreen extends AppCompatActivity {
         setContentView(R.layout.activity_manual_screen);
 
         Forward_BTN = findViewById(R.id.Forward_BTN);
+        Backward_BTN = findViewById(R.id.Backward_BTN);
+        Left_BTN = findViewById(R.id.Left_BTN);
+        Right_BTN = findViewById(R.id.Right_BTN);
 
-        //btAdapter = BluetoothAdapter.getDefaultAdapter(); //Bluetooth definition
+        btAdapter = BluetoothAdapter.getDefaultAdapter(); //Bluetooth definition
     //    System.out.println(btAdapter.getBondedDevices());
-
+        BluetoothDevice hc05 = btAdapter.getRemoteDevice("98:D3:51:F5:B4:73"); //connect to my hc-05 via mac adress
      //   System.out.println(hc05.getName());
 
-   /*     int counter = 0;
+        int counter = 0;
         do {    //creating the socket, if fail, try twice more
             try {
                 btSocket = hc05.createRfcommSocketToServiceRecord(mUUID);
@@ -41,13 +45,12 @@ public class ManualScreen extends AppCompatActivity {
                 e.printStackTrace();
             }
             counter++;
-        } while (!btSocket.isConnected() && counter < 3);*/
+        } while (!btSocket.isConnected() && counter < 3);
 
         try {
             if (btSocket == null) {
                 btAdapter = BluetoothAdapter.getDefaultAdapter();
 
-                BluetoothDevice hc05 = btAdapter.getRemoteDevice("98:D3:51:F5:B4:73"); //connect to my hc-05 via mac adress
                 btSocket = hc05.createInsecureRfcommSocketToServiceRecord(mUUID);
                 BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
                 btSocket.connect();
@@ -58,15 +61,64 @@ public class ManualScreen extends AppCompatActivity {
         }
 
         BluetoothSocket finalBtSocket = btSocket;
-        Forward_BTN.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v) {
-            try {
-                OutputStream outputStream = finalBtSocket.getOutputStream();
-                outputStream.write(1);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+        Forward_BTN.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+
+                    OutputStream outputStream = finalBtSocket.getOutputStream();
+                    outputStream.write(1);
+                    System.out.println(outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
             }
+        });
+
+        Backward_BTN.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+
+                    OutputStream outputStream = finalBtSocket.getOutputStream();
+                    outputStream.write(0);
+                    System.out.println(outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        Right_BTN.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+
+                    OutputStream outputStream = finalBtSocket.getOutputStream();
+                    outputStream.write(2);
+                    System.out.println(outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        Left_BTN.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+
+                    OutputStream outputStream = finalBtSocket.getOutputStream();
+                    outputStream.write(3);
+                    System.out.println(outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
             }
         });
 
@@ -87,12 +139,12 @@ public class ManualScreen extends AppCompatActivity {
 //        }
 
 
-     /*   try {
-            btSocket.close();
-            System.out.println(btSocket.isConnected());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-*/
+//        try {
+//            btSocket.close();
+//            System.out.println(btSocket.isConnected());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
     }
 }
